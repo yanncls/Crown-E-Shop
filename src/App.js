acd4ca97fd831c21fe73d1daedbe9ddc0efa84a7
Yann CLS <cleonisyann@icloud.com>
2022-11-30 15:36:46 +0100

    updating directy page with directory component

--- src/App.js
+++ src/App.js
@@ -1,40 +1,34 @@
+import Directory from "./components/directory/directory.component";
+
 const App = () => {
   const categories = [
     {
       id: 1,
       title: "Casquettes",
+      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
     },
     {
       id: 2,
       title: "Vestes",
+      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
     },
     {
       id: 3,
       title: "Sneakers",
+      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
     },
     {
       id: 4,
       title: "Femmes",
+      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
     },
     {
       id: 5,
       title: "Hommes",
+      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
     },
   ];
-  return (
-    <div className="categories-container">
-      {categories.map(({ title, id }) => (
-        <div className="category-container" key={id}>
-          <div className="background-image">
-            <div className="category-body-container">
-              <h2>{title}</h2>
-              <p>Achetez maintenant</p>
-            </div>
-          </div>
-        </div>
-      ))}
-    </div>
-  );
+  return <Directory categories={categories} />;
 };
 
 export default App;
